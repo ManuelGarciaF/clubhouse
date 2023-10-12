2611c25e7ff09f6d3c85710291f9ad9c2e3ce02f
Manuel Garcia F. <mgarciafrigo@gmail.com>
2023-10-12 15:29:23 -0300

    lint routes file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,26 +1,26 @@
-import express from 'express'
-import * as userController from '../controllers/userController'
-import type { Request, Response } from 'express'
+import express from "express";
+import * as userController from "../controllers/userController";
+import type { Request, Response } from "express";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', function (_req: Request, res: Response) {
-  res.render('index')
-})
+router.get("/", function (_req: Request, res: Response) {
+  res.render("index");
+});
 
 // Register
-router.get('/register', userController.userCreateGet)
-router.post('/register', userController.userCreatePost)
+router.get("/register", userController.userCreateGet);
+router.post("/register", userController.userCreatePost);
 
 // Login
-router.get('/login', userController.userLoginGet)
-router.post('/login', userController.userLoginPost)
+router.get("/login", userController.userLoginGet);
+router.post("/login", userController.userLoginPost);
 
 // Logout
-router.post('/logout', userController.userLogoutPost)
+router.post("/logout", userController.userLogoutPost);
 
 // New Member
-router.get('/join', userController.userJoinGet)
-router.post('/join', userController.userJoinPost)
+router.get("/join", userController.userJoinGet);
+router.post("/join", userController.userJoinPost);
 
-export default router
+export default router;
